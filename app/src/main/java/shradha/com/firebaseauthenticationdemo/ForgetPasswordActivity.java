d0710c672cd98a47720cc879d340290a0cb240b3
shryash12 <dr.shradhasrathod@gmail.com>
2021-06-06 18:14:39 +0800

    ForgetPasswordActivity implemented

--- app/src/main/java/shradha/com/firebaseauthenticationdemo/ForgetPasswordActivity.java
+++ app/src/main/java/shradha/com/firebaseauthenticationdemo/ForgetPasswordActivity.java
@@ -1,70 +1,56 @@
 package shradha.com.firebaseauthenticationdemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Objects;
+
 public class ForgetPasswordActivity extends AppCompatActivity {
-    EditText resetPassword_Email;
-    Button btn_ResetPassword;
-    ProgressBar progressBar;
-    FirebaseAuth mAuth;
+    private EditText editResetPasswordEmail;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
-        resetPassword_Email = findViewById(R.id.email_ResetPass);
-        btn_ResetPassword = findViewById(R.id.btn_ResetPassword);
-        progressBar = findViewById(R.id.progressBar);
-        btn_ResetPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetPassword();
-            }
-        });
+        editResetPasswordEmail = findViewById(R.id.editResetPassword);
+        Button buttonResetPassword = findViewById(R.id.buttonResetPassword);
 
+        buttonResetPassword.setOnClickListener(v -> resetPassword());
+        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.forget_password_screen);
     }
 
     private void resetPassword() {
-        mAuth = FirebaseAuth.getInstance();
-
-        String email = resetPassword_Email.getText().toString();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String email = editResetPasswordEmail.getText().toString();
         if (email.isEmpty()) {
-            resetPassword_Email.setError("email is require");
-            resetPassword_Email.requestFocus();
+            editResetPasswordEmail.setError(getString(R.string.email_is_require));
+            editResetPasswordEmail.requestFocus();
             return;
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            resetPassword_Email.setError("please provide valid email");
-            resetPassword_Email.requestFocus();
+            editResetPasswordEmail.setError(getString(R.string.please_provide_email));
+            editResetPasswordEmail.requestFocus();
             return;
         }
-
-        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(ForgetPasswordActivity.this, new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(ForgetPasswordActivity.this, "check your email to reset the password", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(ForgetPasswordActivity.this, "Try again! something went wrong", Toast.LENGTH_SHORT).show();
-                }
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(ForgetPasswordActivity.this, task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(ForgetPasswordActivity.this, getString(R.string.check_your_email_reset), Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(ForgetPasswordActivity.this, getString(R.string.try_again), Toast.LENGTH_SHORT).show();
             }
-        });
-
-
+        }).addOnFailureListener(this, e ->
+                Toast.makeText(
+                        ForgetPasswordActivity.this,
+                        e.getLocalizedMessage(),
+                        Toast.LENGTH_SHORT).show()
+        );
     }
-
-
 }
